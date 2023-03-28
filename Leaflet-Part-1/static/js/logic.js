2af494e44e4b462a0747b0b000cbcb5a7971b7a3
cmbutler83 <115903147+cmbutler83@users.noreply.github.com>
2023-03-27 23:34:56 -0500

    more

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -41,7 +41,7 @@
   //}
 
     // Perform an API call to the USGS API to get the earthquake information. 
-    d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson").then(function(data){
+    d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson").then(function(data){
     console.log(data.features);
     // Creating a GeoJSON layer with the retrieved data.
     //L.geoJSON(data).addTo(map);
